e1553fdb56c69de072e2300d339adbca812c22cb
ThuLeHoang <thulejoyer@gmail.com>
2019-03-14 13:45:58 +0700

    error DB

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/upload', 'UploadController@index')->name('upload');
+Route::post('upload', 'UploadController@upload');
+
