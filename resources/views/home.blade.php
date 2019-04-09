cb066ed0ea19bddec4f470ee4546cea5f83a849f
ThuLeHoang <thulejoyer@gmail.com>
2019-04-09 14:13:24 +0700

    preview image before upload

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,26 @@
+<style type="text/css">
+	.div-img{
+		background-color: #fff;
+		padding: 2px;
+		margin: 2px;
+		max-height: 200px;
+		
+	}
+	.img{
+		height: 250px;
+		width: 350px;
+	}
+</style>
 @extends('layouts.app')
 <!-- <meta name="csrf-token" content="{{ csrf_token() }}">
 <input type="hidden" name="_token" value="{{ csrf_token() }}"> -->
 @section('content')
 <div id='home'>
-<div class="">
-    <div class="row justify-content-center">
+<div class="container">
+    <div class="row">
         
             @foreach ($Images as $image)
-            <div> <img src="{{ asset('images/' . $image->image) }}" width="300px" height="auto" /> </div>
+            <div class="div-img"> <img class="img" src="{{ asset('images/' . $image->image) }}" /> </div>
             @endforeach
         
     </div>
